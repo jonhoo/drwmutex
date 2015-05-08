3a10947478da6bd8aa451e556fc1954db66485d9
Jon Gjengset <jon@thesquareplanet.com>
2015-05-08 11:48:34 -0400

    Replace sched_*affinity hack with /proc/cpuinfo
    Fixes #4

--- cpus_linux.go
+++ cpus_linux.go
@@ -2,56 +2,44 @@ package drwmutex
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
-	"syscall"
-	"unsafe"
+	"strconv"
+	"strings"
 )
 
 func map_cpus() (cpus map[uint64]int) {
 	cpus = make(map[uint64]int)
 
-	var aff uint64
-	syscall.Syscall(syscall.SYS_SCHED_GETAFFINITY, uintptr(0), unsafe.Sizeof(aff), uintptr(unsafe.Pointer(&aff)))
-
-	n := 0
-	var mask uint64 = 1
-Outer:
-	// move the process to one core at the time, each time determining that
-	// core's CPU ID
-	for {
-		// find the next 1 in the affinity bitmap
-		for (aff & mask) == 0 {
-			mask <<= 1
-			// end if the mask wraps around, or if we've moved past
-			// the highest bit in the affinity bitmap
-			if mask == 0 || mask > aff {
-				break Outer
-			}
-		}
-
-		// lock the process to this core
-		ret, _, err := syscall.Syscall(syscall.SYS_SCHED_SETAFFINITY, uintptr(0), unsafe.Sizeof(mask), uintptr(unsafe.Pointer(&mask)))
-		if ret != 0 {
-			panic(err.Error())
-		}
-
-		// what CPU do we have?
-		c := cpu()
-
-		if oldn, ok := cpus[c]; ok {
-			fmt.Fprintln(os.Stderr, "cpu", n, "==", oldn, "-- both have CPUID", c)
-		}
-
-		cpus[c] = n
-		mask <<= 1
-		n++
+	cpuinfo, err := ioutil.ReadFile("/proc/cpuinfo")
+	if err != nil {
+		return
 	}
 
-	// give process access to all the cores it had access to initially
-	ret, _, err := syscall.Syscall(syscall.SYS_SCHED_SETAFFINITY, uintptr(0), unsafe.Sizeof(aff), uintptr(unsafe.Pointer(&aff)))
-	if ret != 0 {
-		panic(err.Error())
-	}
+	var pnum int
+	var apic uint64
+	lines := strings.Split(string(cpuinfo), "\n")
+	for i, line := range lines {
+		if len(line) == 0 && i != 0 {
+			cpus[apic] = pnum
+			pnum = 0
+			apic = 0
+			continue
+		}
 
+		fields := strings.Fields(line)
+
+		switch fields[0] {
+		case "processor":
+			pnum, err = strconv.Atoi(fields[2])
+		case "apicid":
+			apic, err = strconv.ParseUint(fields[2], 10, 64)
+		}
+
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			return
+		}
+	}
 	return
 }
